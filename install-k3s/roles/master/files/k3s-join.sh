1f69be1d37c6f4b73da8405b9312aacc48d76be8
Alysson Martin <alysson.ti@grupointegrado.br>
2021-11-17 14:27:50 -0300

    Adicionado roles para instalar k3s

---
+++ install-k3s/roles/master/files/k3s-join.sh
@@ -0,0 +1,1 @@
+curl -sfL https://get.k3s.io | K3S_URL=https://161.35.33.135:6443 K3S_TOKEN=K10ebbf83c8c175b29388310b77f584f3b785ea29cf3fbf3e7d40ea8785cd613238::server:1107d8811cd46fae0ad94bf1b9504f76 sh -
